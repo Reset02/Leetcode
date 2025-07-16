baf3f4cdd617c26b416c7563db4d223455723c85
Reset02 <lulu30319@gmail.com>
2025-07-16 12:59:04 +0800

    Time: 61 ms (61.24%), Space: 39.2 MB (33.33%) - LeetHub

--- 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
+++ 3201-find-the-maximum-length-of-valid-subsequence-i/3201-find-the-maximum-length-of-valid-subsequence-i.py
@@ -1,10 +1,24 @@
 class Solution:
     def maximumLength(self, nums: List[int]) -> int:
-        res = 0
-        for pattern in [[0, 0], [0, 1], [1, 0], [1, 1]]:
-            cnt = 0
-            for num in nums:
-                if num % 2 == pattern[cnt % 2]:
-                    cnt += 1
-            res = max(res, cnt)
-        return res
+        odds = []
+        evens = []
+
+        for num in nums:
+            if num % 2 == 0:
+                evens.append(num)
+            else:
+                odds.append(num)
+
+        # Case 1: All odds → 所有奇數形成的序列是合法的（因為 (奇+奇)%2 == 0）
+        # Case 2: All evens → (偶+偶)%2 == 0
+        max_same = max(len(odds), len(evens))
+
+        # Case 3: Alternating 奇偶奇偶奇偶 → (奇+偶)%2 == 1, (偶+奇)%2 == 1
+        max_alternating = 1
+        last = nums[0] % 2
+        for i in range(1, len(nums)):
+            if nums[i] % 2 != last:
+                max_alternating += 1
+                last = nums[i] % 2
+
+        return max(max_same, max_alternating)
