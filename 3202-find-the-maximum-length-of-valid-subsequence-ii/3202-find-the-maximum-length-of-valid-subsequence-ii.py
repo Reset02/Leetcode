264d017cf35bec9075e2752dd6a7aa37cd7b2804
Reset02 <lulu30319@gmail.com>
2025-07-17 18:25:11 +0800

    Time: 2424 ms (39.34%), Space: 25.5 MB (58.2%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,10 +1,13 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
         dp = [[0] * k for _ in range(k)]
-        res = 0
+        res = 1  # 單個元素本身就是長度為 1 的合法子序列
+
         for num in nums:
-            num %= k
+            curr = num % k
             for prev in range(k):
-                dp[prev][num] = dp[num][prev] + 1
-                res = max(res, dp[prev][num])
+                # 嘗試把 num 接在以 dp[curr][prev] 結尾的序列之後
+                dp[prev][curr] = max(dp[prev][curr], dp[curr][prev] + 1)
+                res = max(res, dp[prev][curr])
+
         return res
