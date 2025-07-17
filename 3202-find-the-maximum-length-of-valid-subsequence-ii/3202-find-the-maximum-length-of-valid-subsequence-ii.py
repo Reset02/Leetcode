da74403fe3aad2861a9f2ea485887789d9dc1201
Reset02 <lulu30319@gmail.com>
2025-07-17 18:21:08 +0800

    Time: 1366 ms (76.23%), Space: 25.6 MB (46.72%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,26 +1,10 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        dp = defaultdict(dict) # dp[mod][last_num] = max_length
-        res = 1
-
-        for i in range(len(nums)):
-            curr = nums[i]
-            local_updates = defaultdict(int)
-
-            for j in range(i):
-                prev = nums[j]
-                mod = (prev + curr) % k
-                prev_len = dp[mod].get(prev, 1)
-                local_updates[(mod, curr)] = max(local_updates[(mod, curr)], prev_len + 1)
-                res = max(res, local_updates[(mod, curr)])
-            
-
-            # 將更新套入 dp
-            for (mod, num), length in local_updates.items():
-                dp[mod][num] = max(dp[mod].get(num, 1), length)
-            
-            # 單一元素本身也可以成為長度為 1 的 subsequence
-            for mod in range(k):
-                dp[mod].setdefault(curr, 1)
-        
+        dp = [[0] * k for _ in range(k)]
+        res = 0
+        for num in nums:
+            num %= k
+            for prev in range(k):
+                dp[prev][num] = dp[num][prev] + 1
+                res = max(res, dp[prev][num])
         return res
