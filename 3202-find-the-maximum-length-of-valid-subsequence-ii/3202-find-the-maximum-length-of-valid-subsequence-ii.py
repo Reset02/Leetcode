526428165aae26b5d3c3e014e62ba3052207d288
Reset02 <lulu30319@gmail.com>
2025-07-17 18:24:47 +0800

    Time: 1407 ms (73.77%), Space: 25.9 MB (23.77%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,23 +1,10 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        dp = defaultdict(dict)  # dp[mod][last_num] = max length
-        res = 1
-
-        for i in range(len(nums)):
-            curr = nums[i]
-            local = defaultdict(int)
-
-            for j in range(i):
-                prev = nums[j]
-                mod = (prev + curr) % k
-                prev_len = dp[mod].get(prev, 1)
-                local[(mod, curr)] = max(local[(mod, curr)], prev_len + 1)
-                res = max(res, local[(mod, curr)])
-
-            for (mod, num), length in local.items():
-                dp[mod][num] = max(dp[mod].get(num, 1), length)
-
-            for mod in range(k):
-                dp[mod].setdefault(curr, 1)
-
+        dp = [[0] * k for _ in range(k)]
+        res = 0
+        for num in nums:
+            num %= k
+            for prev in range(k):
+                dp[prev][num] = dp[num][prev] + 1
+                res = max(res, dp[prev][num])
         return res
