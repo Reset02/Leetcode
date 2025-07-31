1f91bbdfe6ec9c9e108cac387eecd0d5f5e3021e
Reset02 <lulu30319@gmail.com>
2025-07-31 20:17:16 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& arr) {
+        unordered_set<int> result;
+        set<int> prev;// 使用 set 自動去重，也可以用 unordered_set
+
+        for (int num : arr){
+            set<int> cur;
+            cur.insert(num);
+            for (int x: prev){
+                cur.insert(x | num);
+            }
+            prev = cur;
+            for (int x : cur){
+                result.insert(x);
+            }
+        }
+        return result;
+    } 
+};
