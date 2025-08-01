71f648f69b430ecd6591c2c3682edc483a184ac9
Reset02 <lulu30319@gmail.com>
2025-08-01 17:19:50 +0800

    Time: 0 ms (100%), Space: 17.9 MB (16.18%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -3,7 +3,7 @@ class Solution:
         res = []
         for i in range(numRows):
             row = [1] * (i + 1) # 頭尾都是 1，中間預設先填 1
-            for j in range(1, i): # 從第二個元素到倒數第二個
-                row[j] = res[i - 1][j - 1] + res[i- 1][j]
+            for j in range(1, i): # 從第二個元素到倒數第二個 當 i = 2，只會有一個中間值（j=1）
+                row[j] = res[i - 1][j - 1] + res[i - 1][j]
             res.append(row)
         return res
