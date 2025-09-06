143db2b069fceb4ea56c8d3ee068573b3d6a0791
Reset02 <lulu30319@gmail.com>
2025-09-06 10:12:47 +0800

    Time: 4311 ms (5.16%), Space: 54.3 MB (77.38%) - LeetHub

--- 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.py
+++ 3495-minimum-operations-to-make-array-elements-zero/3495-minimum-operations-to-make-array-elements-zero.py
@@ -1,22 +1,16 @@
 class Solution:
-    def minOperations(self, queries: List[List[int]]) -> int:
-        # 計算 [1, x] 的總步數
-        def totalSteps(x: int) -> int:
-            if x <= 0:
-                return 0
-            steps_sum = 0
-            # 每層區間: [4^k, 4^(k+1)-1]
-            k = 0
-            while (1 << (2*k)) <= x:  # 4^k = 2^(2k)
-                left = 1 << (2*k)          # 4^k
-                right = min(x, (1 << (2*(k+1))) - 1)  # min(x, 4^(k+1)-1)
-                count = right - left + 1
-                steps_sum += count * (k + 1)
-                k += 1
-            return steps_sum
+    def get(self, num: int) -> int:
+        i = 1
+        base = 1
+        cnt = 0
+        while base <= num:
+            cnt += ((i + 1) // 2) * (min(base * 2 - 1, num) - base + 1)
+            i += 1
+            base *= 2
+        return cnt
 
-        ans = 0
-        for l, r in queries:
-            total_steps = totalSteps(r) - totalSteps(l - 1)
-            ans += (total_steps + 1) // 2
-        return ans
+    def minOperations(self, queries: List[List[int]]) -> int:
+        res = 0
+        for q in queries:
+            res += (self.get(q[1]) - self.get(q[0] - 1) + 1) // 2
+        return res
