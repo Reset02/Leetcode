283fc48ee8a9d17539705538a03b1fcf34805f4c
Reset02 <lulu30319@gmail.com>
2025-10-01 13:56:16 +0800

    Time: 0 ms (100%), Space: 7.7 MB (80.28%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int consumed_bottles = 0;
+        int ans = numBottles; // 一開始先喝掉所有瓶子
+        int empty = numBottles; // 全部變成空瓶
 
-        while (numBottles >= numExchange){
-            // Maximum number of times we can consume numExchange
-            // number of bottles using numBottles.
-            int k = numBottles / numExchange;
-
-            consumed_bottles += numExchange * k;
-            numBottles -= numExchange * k;
-
-            numBottles += k;
+        while (empty >= numExchange){
+            int newFull = empty / numExchange; // 可以換的新瓶
+            ans += newFull; // 喝掉這些新瓶
+            empty = (empty % numExchange) + newFull; // 更新空瓶數
         }
-        return consumed_bottles + numBottles;
+        return ans;
     }
 };
