70cde6920d407d116972432be88a256385d74316
Reset02 <lulu30319@gmail.com>
2025-11-10 12:15:43 +0800

    Time: 210 ms (71.88%), Space: 29.7 MB (90.63%) - LeetHub

--- 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.py
+++ 3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.py
@@ -1,12 +1,17 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        s = []
-        res = 0
+        s = []      # 單調遞增堆疊
+        res = 0     # 操作次數
         for a in nums:
+            # 若當前數字比堆疊頂還小，就把較大的彈出
             while s and s[-1] > a:
                 s.pop()
+
+            # 若是 0，不影響後續操作
             if a == 0:
                 continue
+
+            # 若堆疊空 或 目前值比堆疊頂大 → 代表出現新的高度層
             if not s or s[-1] < a:
                 res += 1
                 s.append(a)
