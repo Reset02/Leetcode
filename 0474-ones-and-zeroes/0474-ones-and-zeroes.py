616a00de482920526e3a9336c26b9e53a60e3af7
Reset02 <lulu30319@gmail.com>
2025-11-11 17:16:48 +0800

    Time: 1663 ms (46.08%), Space: 18.1 MB (72.1%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMaxForm(self, strs, m, n):
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for s in strs:
+            zeros = s.count('0')
+            ones = s.count('1')
+            
+            # 從後往前更新，避免重複使用同一個字串
+            for i in range(m, zeros - 1, -1):
+                for j in range(n, ones - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+        
+        return dp[m][n]
